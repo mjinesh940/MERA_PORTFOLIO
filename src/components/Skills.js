19fd7aff3949cbd51096f56c46f5c2dfce0d30a4
Jinesh Modi <mjinesh940@gmail.com>
2025-05-24 18:40:07 +0530

    Added Swiper to Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import JavaImg from '../images/JAVA.png';
 import ReactImg from '../images/React.png';
 import SpringImg from '../images/Spring.png';
@@ -8,8 +14,8 @@ import HibernateImg from '../images/Hibernate.png';
 import WebTechImg from '../images/WebTech.png';
 import PHPImg from '../images/PHP.png';
 import ABAP from '../images/ABAP.png';
+
 import './Skills.css';
-import { ImPrevious } from 'react-icons/im';
 
 const skills = [
   { name: 'Java', img: JavaImg },
@@ -27,13 +33,30 @@ const Skills = () => {
   return (
     <section className="skills-section">
       <h2 className="skills-title">Skills</h2>
-      <div className="skills-container">
-        {skills.map((skill, index) => (
-          <div key={index} className="skill">
-            <img src={skill.img} alt={skill.name} className="skill-icon" />
-            <div className="skill-name">{skill.name}</div>
-          </div>
-        ))}
+      <div className="skills-swiper-container">
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={30}
+          navigation
+          pagination={{ clickable: true }}
+          loop={true}
+          autoplay={{ delay: 2500, disableOnInteraction: false }}
+          slidesPerView={1}
+          breakpoints={{
+            640: { slidesPerView: 2, spaceBetween: 20 },
+            768: { slidesPerView: 3, spaceBetween: 30 },
+            1024: { slidesPerView: 4, spaceBetween: 40 },
+          }}
+        >
+          {skills.map((skill, index) => (
+            <SwiperSlide key={index}>
+              <div className="skill">
+                <img src={skill.img} alt={skill.name} className="skill-icon" />
+                <div className="skill-name">{skill.name}</div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
